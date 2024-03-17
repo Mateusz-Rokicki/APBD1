a516154d4b98b301f0ea3b0a9b6d51823e28df48
s28001 <s28001@pjwstk.edu.pl>
2024-03-17 13:58:59 +0100

    feature-average

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int[] tab = { 1, 2, 3, 4, 5 };
+int[] tab = { 1, 2, 3, 4, 5, 6 , 7, 8};
 Console.WriteLine("Average: "+Average(tab));
 
 
