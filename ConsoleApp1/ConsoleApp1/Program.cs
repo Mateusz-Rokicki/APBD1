1d7db6919c683fc895cfb4c664c8d18e54be1089
s28001 <s28001@pjwstk.edu.pl>
2024-03-17 14:35:49 +0100

    feature-max

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,7 +2,7 @@
 
 int[] tab = { 1, 2, 3, 4, 5, 6 , 7, 8};
 Console.WriteLine("Average: "+Average(tab));
-
+Console.WriteLine("Max: "+MaxValue(tab));
 
 
 static double Average(int[] tab)
@@ -13,4 +13,9 @@ static double Average(int[] tab)
         sum+=tab[i];
     }
     return sum/tab.Length;
+}
+
+static int MaxValue(int[] tab)
+{
+    return tab.Max();
 }
